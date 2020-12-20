b09a3be132dd03e846ef13d5f62e9c6cf6bd55ed
Sabbir <mdsabbirahmed25@gmail.com>
2020-12-20 17:30:52 +0600

    Added swipe refresh layout and function

--- app/src/main/java/com/journeyloginc/invisibleapps/ui/home/HomeFragment.java
+++ app/src/main/java/com/journeyloginc/invisibleapps/ui/home/HomeFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +14,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.journeyloginc.invisibleapps.R;
 
@@ -18,18 +22,37 @@ public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
+    //webview
+    public WebView mWebView;
+    private SwipeRefreshLayout swipeRefreshLayout;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        View v=inflater.inflate(R.layout.fragment_home, container, false);
+        mWebView = (WebView) v.findViewById(R.id.webview_home);
+        swipeRefreshLayout = (SwipeRefreshLayout) v.findViewById(R.id.swipe);
+        mWebView.loadUrl("https://journeyloginc.com");
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.getSettings().setSupportZoom(true);
+        mWebView.getSettings().setAppCacheEnabled(true);
+        // on refresh
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onRefresh() {
+                mWebView.reload();
             }
         });
-        return root;
+        //on refresh end
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.setWebViewClient(new WebViewClient() {
+            public void onPageFinished(WebView mWebView, String url){
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+        return v;
     }
 }
