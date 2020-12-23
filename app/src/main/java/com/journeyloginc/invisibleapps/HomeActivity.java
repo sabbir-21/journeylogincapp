02614eb1319776411d47d33452535a8f7c0945cd
Sabbir <mdsabbirahmed25@gmail.com>
2020-12-23 20:27:20 +0600

    Progress dialog and some error fix

--- app/src/main/java/com/journeyloginc/invisibleapps/HomeActivity.java
+++ app/src/main/java/com/journeyloginc/invisibleapps/HomeActivity.java
@@ -1,21 +1,29 @@
 package com.journeyloginc.invisibleapps;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class HomeActivity extends AppCompatActivity {
 
         private WebView webView;
+        private ProgressBar progressBar;
+        private ProgressDialog pd;
+        String weburl = "https://journeyloginc.com";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         webView = (WebView) findViewById(R.id.webview);
-        webView.loadUrl("https://journeyloginc.com");
+        webView.loadUrl(weburl);
 
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -36,5 +44,63 @@ public class HomeActivity extends AppCompatActivity {
         webSettings.setSaveFormData(true);
         webSettings.setEnableSmoothTransition(true);
 
+        //progress bar
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        webView.setWebChromeClient(new WebChromeClient() {
+            // page loading progress, gone when fully loaded
+            public void onProgressChanged(WebView view, int progress) {
+
+                if (progress < 100 && progressBar.getVisibility() == ProgressBar.GONE) {
+                    progressBar.setVisibility(ProgressBar.VISIBLE);
+                }
+
+                if (progress == 100) {
+                    progressBar.setVisibility(ProgressBar.GONE);
+                }
+                progressBar.setProgress(progress);
+
+
+            }
+        });
+
+        //progress dialog
+        pd = new ProgressDialog(HomeActivity.this);
+        pd.setMessage("Loading Please Wait...");
+
+        webView.setWebViewClient(new pdjr(pd));
+        pd.setCanceledOnTouchOutside(false);
+    }
+    public class pdjr extends WebViewClient{
+        ProgressDialog pd;
+        public pdjr (ProgressDialog pd){
+            this.pd = pd;
+            pd.show();
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(weburl);
+            return super.shouldOverrideUrlLoading(view, weburl);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            if(pd.isShowing()){
+                pd.dismiss();
+            }
+
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else {
+            super.onBackPressed();
+        }
     }
 }
