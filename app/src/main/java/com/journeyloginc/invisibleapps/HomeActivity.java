7e39bad9bfe8e1d57b9b590967d651d8fb1cebd7
Sabbir <mdsabbirahmed25@gmail.com>
2020-12-23 19:51:21 +0600

    Added inner webview and web settings

--- app/src/main/java/com/journeyloginc/invisibleapps/HomeActivity.java
+++ app/src/main/java/com/journeyloginc/invisibleapps/HomeActivity.java
@@ -1,52 +1,40 @@
 package com.journeyloginc.invisibleapps;
 
-        import android.os.Bundle;
-        import android.view.Menu;
-
-        import com.google.android.material.navigation.NavigationView;
-
-        import androidx.navigation.NavController;
-        import androidx.navigation.Navigation;
-        import androidx.navigation.ui.AppBarConfiguration;
-        import androidx.navigation.ui.NavigationUI;
-        import androidx.drawerlayout.widget.DrawerLayout;
-        import androidx.appcompat.app.AppCompatActivity;
-        import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private AppBarConfiguration mAppBarConfiguration;
-
+        private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_blogs, R.id.nav_Create_Post, R.id.nav_login)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
-    }
+        webView = (WebView) findViewById(R.id.webview);
+        webView.loadUrl("https://journeyloginc.com");
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+
+        webView.setWebViewClient(new WebViewClient());
+        //user cookies
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setSavePassword(true);
+        webSettings.setSaveFormData(true);
+        webSettings.setEnableSmoothTransition(true);
+
     }
 }
--- app/src/main/java/com/journeyloginc/invisibleapps/MainActivity.java
+++ app/src/main/java/com/journeyloginc/invisibleapps/MainActivity.java
@@ -4,18 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.SystemClock;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static int Splash_time = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //splash code
 
-        SystemClock.sleep(3000);
-        Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
-        startActivity(homeIntent);
-        finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        }, Splash_time);
+
+
+
     }
 }
